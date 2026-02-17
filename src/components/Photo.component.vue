aebf4bf3a99320a75d8ecbef885fb37bc15ef104
Eli <bernerielia@gmail.com>
2026-02-17 08:52:49 +0100

    fix: photo component

--- src/components/Photo.component.vue
+++ src/components/Photo.component.vue
@@ -9,7 +9,7 @@
 </script>
 
 <template>
-    <div class="fixed bg-[rgba(0,0,0,0.5)] z-50 flex items-center justify-center w-full h-full" @click.self="$emit('close')">
+    <div class="fixed bg-[rgba(0,0,0,0.5)] z-50 inset-0 flex items-center justify-center w-full h-full" @click.self="$emit('close')">
         <img :src="photo.url" :alt="photo.name" class=" max-w-3/4 max-h-3/4 object-contain rounded-2xl" />
     </div>
 </template>
