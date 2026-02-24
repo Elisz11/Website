b27a70b27fa9bf36a3a090f12089005ce79a6bf7
Eli <bernerielia@gmail.com>
2026-02-24 23:18:56 +0100

    fix: footer links

--- src/components/Footer.component.vue
+++ src/components/Footer.component.vue
@@ -15,6 +15,6 @@
 
 <template>
     <main class="flex w-full p-2 gap-2 justify-center cursor-pointer">
-        <a class="hover:underline active:scale-95 transition-all" v-for="link in links">{{ link.name }}</a>
+        <a class="hover:underline active:scale-95 transition-all" v-for="link in links" :href="link.url">{{ link.name }}</a>
     </main>
 </template>
